34a102efa4cf2c21a45597e132019ed6aa6d1f4b
Carlos Santiago Becerra García <santiago1@javerianacali.edu.co>
2025-08-18 22:35:25 -0500

    Segundo commit: agregar funciones restar() y dividir()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stdexcept>
 #include "matematicas.h"
 
 int main() {
-    int a = 8, b = 6;
-    std::cout << "Suma: " << sumar(a, b) << "\n";
+    int a = 12, b = 3;
+    std::cout << "Suma: "  << sumar(a, b)   << "\n";
+    std::cout << "Resta: " << restar(a, b)  << "\n";
+    try {
+        std::cout << "Dividir: " << dividir(a, b) << "\n";
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+    }
     return 0;
 }
--- matematicas.h
+++ matematicas.h
@@ -2,5 +2,7 @@
 #define MATEMATICAS_H
 
 int sumar(int a, int b);
+int restar(int a, int b);
+double dividir(double a, double b);
 
 #endif // MATEMATICAS_H
